a683af183d89bd17a6cfdc7d47ac4717eb28aeb2
Brian Cox <brian90254@gmail.com>
2025-07-18 11:21:50 -0700

    Create SynthAnthro_MeasureMesh_1.py

---
+++ Tools/SyntheticAnthropometry/CAESAR/src/SynthAnthro_MeasureMesh_1.py
@@ -0,0 +1,21 @@
+# BRIAN COX copyright 2025
+#
+# EXAMPLE TO RUN THIS SCRIPT:
+# ---------------------------
+# IN TERMINAL, NAVIGATE TO THE PROJECT FOLDER
+#   cd Dropbox/GitHub/MTWABP/Tools/SyntheticAnthropometry/CAESAR
+# IF NEEDED, COMMAND TO MAKE A VENV:
+#   python3.9 -m venv venv39
+# ACTIVATE THE VENV:
+#   source venv39/bin/activate
+# IF NEEDED, COMMAND TO DOWNLOAD ALL REQS FOR THIS PROJECT:
+#   pip3 install -r requirements.txt
+# THEN RUN THE CODE IN "src"
+#   python src/SynthAnthro_MeasureMesh_1.py
+
+import cv2
+import numpy as np
+from datetime import datetime
+import os
+import math
+import sys
