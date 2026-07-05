897dbe7ed6992f60d832f1e56eb5df295ba134d5
Brian Cox <brian90254@gmail.com>
2026-07-05 12:18:12 -0700

    update

--- FunAndGames/GODOT/Tutorial_1/Scripts/coin.gd
+++ FunAndGames/GODOT/Tutorial_1/Scripts/coin.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+@onready var game_manager: Node = %GameManager
 
 ## Called when the node enters the scene tree for the first time.
 #func _ready() -> void:
@@ -14,6 +15,7 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	#pass # Replace with function body.
+	game_manager.add_point()
 	print ("+1 coin")
 	queue_free()
 	
